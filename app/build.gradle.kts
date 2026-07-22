3ff4a137afafc06d38cc108e3f2ca838070446b4
juanmaacontreras <juacontreras@frba.utn.edu.ar>
2026-07-22 20:17:24 -0300

    v 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,4 +33,5 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
 }
